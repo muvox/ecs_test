17e3540a4ac316d48c7b50012b8be233911a9174
Joni Niemi <muvoksi@gmail.com>
2019-12-06 14:23:08 +0200

    trying to compile on windowsWIP

---
+++ CMakeFiles/ECS_TEST.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/dev/projects/ecs_test/src/platform/generic/cmwc4096.c" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/platform/generic/cmwc4096.c.obj"
+  "C:/dev/projects/ecs_test/src/rendering/staticLibs/stb_image.c" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/rendering/staticLibs/stb_image.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "headers"
+  "sources"
+  "src"
+  "lib/glew/include"
+  "lib/SDL2/include"
+  "lib/Assimp/include"
+  "lib/libtcod/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/dev/projects/ecs_test/src/core/memory.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/core/memory.cpp.obj"
+  "C:/dev/projects/ecs_test/src/dataStructures/string.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/dataStructures/string.cpp.obj"
+  "C:/dev/projects/ecs_test/src/ecs/ecs.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/ecs/ecs.cpp.obj"
+  "C:/dev/projects/ecs_test/src/ecs/ecsComponent.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/ecs/ecsComponent.cpp.obj"
+  "C:/dev/projects/ecs_test/src/ecs/ecsSystem.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/ecs/ecsSystem.cpp.obj"
+  "C:/dev/projects/ecs_test/src/main.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/main.cpp.obj"
+  "C:/dev/projects/ecs_test/src/math/aabb.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/math/aabb.cpp.obj"
+  "C:/dev/projects/ecs_test/src/math/color.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/math/color.cpp.obj"
+  "C:/dev/projects/ecs_test/src/math/matrix.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/math/matrix.cpp.obj"
+  "C:/dev/projects/ecs_test/src/math/plane.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/math/plane.cpp.obj"
+  "C:/dev/projects/ecs_test/src/math/quaternion.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/math/quaternion.cpp.obj"
+  "C:/dev/projects/ecs_test/src/math/sphere.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/math/sphere.cpp.obj"
+  "C:/dev/projects/ecs_test/src/math/transform.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/math/transform.cpp.obj"
+  "C:/dev/projects/ecs_test/src/math/vecmath.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/math/vecmath.cpp.obj"
+  "C:/dev/projects/ecs_test/src/math/vector.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/math/vector.cpp.obj"
+  "C:/dev/projects/ecs_test/src/platform/generic/genericMemory.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/platform/generic/genericMemory.cpp.obj"
+  "C:/dev/projects/ecs_test/src/platform/opengl/openGLRenderDevice.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/platform/opengl/openGLRenderDevice.cpp.obj"
+  "C:/dev/projects/ecs_test/src/platform/sdl/sdlApplication.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/platform/sdl/sdlApplication.cpp.obj"
+  "C:/dev/projects/ecs_test/src/platform/sdl/sdlWindow.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/platform/sdl/sdlWindow.cpp.obj"
+  "C:/dev/projects/ecs_test/src/platform/sdl/sdltiming.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/platform/sdl/sdltiming.cpp.obj"
+  "C:/dev/projects/ecs_test/src/rendering/arrayBitmap.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/rendering/arrayBitmap.cpp.obj"
+  "C:/dev/projects/ecs_test/src/rendering/ddstexture.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/rendering/ddstexture.cpp.obj"
+  "C:/dev/projects/ecs_test/src/rendering/indexedModel.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/rendering/indexedModel.cpp.obj"
+  "C:/dev/projects/ecs_test/src/rendering/modelLoader.cpp" "C:/dev/projects/ecs_test/CMakeFiles/ECS_TEST.dir/src/rendering/modelLoader.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "headers"
+  "sources"
+  "src"
+  "lib/glew/include"
+  "lib/SDL2/include"
+  "lib/Assimp/include"
+  "lib/libtcod/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ECS_TEST.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ECS_TEST.
+# This may be replaced when dependencies are built.
